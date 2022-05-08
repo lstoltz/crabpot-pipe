0bb3ef452ab3bc9693bdae667723de45850d1b02
Linus Stoltz <lstoltz5@gmail.com>
2022-05-08 11:57:35 -0700

    hide user info from web

--- email_alert_send.py
+++ email_alert_send.py
@@ -9,33 +9,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 subject = "Review Requested for Time Series"
-body = "File could not be automatically processed please remove bad data and send back"
-sender_email = "chrp.crabpot@gmail.com"
-receiver_email = "lstoltz5@gmail.com"
+body="""\
+    <html>
+        <head></head>
+        <body>
+            File could not be automatically processed: <br>
+            Please delete rows with bad data and send back file as an attachment. <br>
+            <b> Caution, do not modify filename! </b>
+    </html>
+    """
+
+sender_email = os.getenv("EMAIL_USER")
+receiver_email = [os.getenv("SENDER_LIST")]
+print(receiver_email)
 password = os.getenv("APP_PASS")
-
-# port = 587  # For starttls
-# smtp_server = "smtp.gmail.com"
-# message = """\
-# Subject: Hi there
-
-# This message is sent from Python."""
-
-# # Create a secure SSL context
-# context = ssl.create_default_context()
-# with smtplib.SMTP(smtp_server, port) as server:
-#     server.ehlo()  # Can be omitted
-#     server.starttls(context=context)
-#     server.ehlo()  # Can be omitted
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, message)
 
 message = MIMEMultipart()
 message["From"] = sender_email
-message["To"] = receiver_email
+message["To"] = ", ".join(receiver_email)
 message["Subject"] = subject
 
-message.attach(MIMEText(body, "plain"))
+message.attach(MIMEText(body, "html"))
 
 filename = "2002048_mla_20210616_102506_DissolvedOxygen.csv"
 
